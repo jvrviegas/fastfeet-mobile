f39710dcc5b145938d8e6227680f8d635392f48d
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-17 14:43:27 -0300

    D - Sign In Page - Add sign in dispatch

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/fastfeet-white-logo.png';
 
-import Background from '~/components/Background';
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
-  function handleSubmit() {}
+  const [deliverymanId, setDeliverymanId] = useState('');
+  const loading = useSelector((state) => state.auth.loading);
+
+  const dispatch = useDispatch();
+
+  function handleSubmit() {
+    dispatch(signInRequest(deliverymanId));
+  }
 
   return (
-    <Background>
-      <Container>
-        <Image source={logo} />
+    <Container>
+      <Image source={logo} />
 
-        <Form>
-          <FormInput
-            icon="person-outline"
-            keyboardType="email-address"
-            autoCorrect={false}
-            autoCapitalize="none"
-            placeholder="Informe seu ID de cadastro"
-            returnKeyType="send"
-            onSubmitEditing={handleSubmit}
-          />
-          <SubmitButton onPress={handleSubmit}>Entrar no sistema</SubmitButton>
-        </Form>
-      </Container>
-    </Background>
+      <Form>
+        <FormInput
+          icon="person-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Informe seu ID de cadastro"
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+          value={deliverymanId}
+          onChangeText={setDeliverymanId}
+        />
+        <SubmitButton loading={loading} onPress={handleSubmit}>
+          Entrar no sistema
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
