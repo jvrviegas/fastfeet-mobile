d068ada495226ad6a9e2db92130e71624683f3c7
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-04-17 14:35:52 -0300

    M - Index - Add Persist Gate and Provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
+import { store, persistor } from './store';
 import Routes from './routes';
 
 // import { Container } from './styles';
@@ -10,8 +14,12 @@ import Routes from './routes';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#6836BF" />
-      <Routes />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#6836BF" />
+          <Routes />
+        </PersistGate>
+      </Provider>
     </>
   );
 }
